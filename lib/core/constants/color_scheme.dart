91c173e09105d31080fd86d74efcec3d00394fd5
Harshit <hrpofficial736@gmail.com>
2025-01-11 23:56:24 +0530

    built splash screen

---
+++ lib/core/constants/color_scheme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppColorScheme {
+
+  // Background Colors
+  final Color primaryBackgroundColor = Color(0xFF121212);
+  final Color secondaryBackgroundColor = Color(0xFF1E1E1E);
+
+
+  // Accent Colors
+  final Color primaryAccentColor = Color(0xFFFF5722);
+  final Color secondaryAccentColor = Color(0xFFFFEB3B);
+
+
+  // Status Colors
+  final Color successIndicatorColor = Color(0xFF4CAF50);
+  final Color warningIndicatorColor = Color(0xFFF44336);
+
+
+  // Text Colors 
+  final Color primaryTextColor = Color(0xFFE0E0E0);
+  final Color secondaryTextColor = Color(0xFFB0B0B0);
+  final Color linkTextColor = Color(0xFFFFEB3B);
+
+
+  // Additional Colors
+  final Color containerColor = Color(0xFF2C2C2C);
+  final Color dividerColor = Color(0xFF333333);
+}
